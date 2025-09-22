89760e0e81913a893b468a145815ad9583aeb105
Sonja Ojala <sojala@c2r1p3.hive.fi>
2025-09-22 11:57:36 +0300

    Fixed cpp08 ex02 so MutantStack works. Have to solidify understanding

--- cpp08/ex02/inc/MutantStack.hpp
+++ cpp08/ex02/inc/MutantStack.hpp
@@ -17,3 +17,15 @@ class MutantStack : public std::stack<T>
 		iterator	begin(void);
 		iterator	end(void);
 };
+
+template <typename T>
+typename MutantStack<T>::iterator	MutantStack<T>::begin(void)
+{
+	return this->c.begin();
+}
+
+template <typename T>
+typename MutantStack<T>::iterator	MutantStack<T>::end(void)
+{
+	return	this->c.end();
+}
